30d54c14486d8cd1d497e52353c2948e9908b18b
Kostya <betenya.kostya@mail.ru>
2024-08-18 22:14:41 +0300

    create node.js script which build fasm project

---
+++ build.js
@@ -0,0 +1,38 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+console.log("10% - Read setup.json;");
+const config = JSON.parse(fs.readFileSync('setup.json', 'utf8'));
+
+console.log("20% - Check build parameters;");
+const buildType = process.argv[2];
+if (!buildType || (buildType !== 'Debug' && buildType !== 'Release')) {
+    console.error("Usage: node build.js <Debug|Release>");
+    process.exit(1);
+}
+
+console.log("35% - Branching by parameter;");
+const buildConfig = config[buildType.toLowerCase()];
+if (!buildConfig) {
+    console.error(`Unknown build type: ${buildType}`);
+    process.exit(1);
+}
+
+try {
+    console.log(`50% - Switching to the compiler directory: ${process.cwd()};`);
+    process.chdir(config.fasmPath);
+
+    console.log(`55% - Try to build`);
+    const command = `fasm ${buildConfig.input} ${buildConfig.output}`;
+    execSync(command, { stdio: 'inherit' });
+    console.log(`85% - Success`);
+
+    console.log(`90% - Return in work directory: ${process.cwd()};`);
+    process.chdir(config.projectDir);
+    console.log(`Complete with exit code 0.`);
+    process.exit(0);
+} catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+}

c0720945c4e417bb3cdce6d46ad0e2bba7d9dbaa
Kostya <betenya.kostya@mail.ru>
2024-08-25 22:27:17 +0300

    change coords convertot

--- scripts/Getters.asm
+++ scripts/Getters.asm
@@ -32,27 +32,138 @@ proc GetGLfloatColor\
 endp
 
 ; =================== Rect =================== ;
-proc Scripts.Getters.ConvertRect uses eax ebx ecx,\
-     resAdress
+proc Scripts.Getters.ConvertCoords uses eax ebx ecx edi,\
+     coords, resAdress
     locals
-        multiplier  dd  4
+        multiplierRect    dd  4
     endl
-    mov     ebx, [resAdress]
-    mov     eax, [ebx]
-    mul     dword [multiplier]
-    xchg    ecx, eax
-    add     ebx, 4
-    .convert:
-        mov     eax, [ebx]
-        stdcall GetXGLfloatCoord, eax
-        mov     [ebx], eax
+    mov     ebx, [coords]
+    mov     edi, [resAdress]
+    .rect:
+        mov     ecx, [ebx]
+        mov     [edi], ecx
+        add     ebx, 4
+        cmp     ecx, 0
+        je      .circle
+        add     edi, 4
+        .convertRect:
+            mov     eax, [ebx]
+            stdcall GetXGLfloatCoord, eax
+            mov     [edi], eax
+            mov     [edi + 8], eax
 
-        mov     eax, [ebx + 4]
-        stdcall GetYGLfloatCoord, eax
-        mov     [ebx + 4], eax
+            mov     eax, [ebx + 8]
+            stdcall GetXGLfloatCoord, eax
+            mov     [edi + 16], eax
+            mov     [edi + 24], eax
 
-        add     ebx, 8
-        loop    .convert
+            mov     eax, [ebx + 4]
+            stdcall GetYGLfloatCoord, eax
+            mov     [edi + 4], eax
+            mov     [edi + 28], eax
+
+            mov     eax, [ebx + 12]
+            stdcall GetYGLfloatCoord, eax
+            mov     [edi + 12], eax
+            mov     [edi + 20], eax
+
+            add     ebx, 16
+            add     edi, 32
+            loop    .convertRect
+    .circle:
+        mov     edi, [resAdress]
+        add     edi, 164
+        mov     ecx, [ebx]
+        mov     [edi], ecx
+        cmp     ecx, 0
+        je      .exit
+        add     ebx, 4
+        add     edi, 4
+        .convertCircle:
+            push    ecx
+            ; center
+            mov     eax, [ebx]
+            stdcall GetXGLfloatCoord, eax
+            mov     [edi], eax
+            mov     eax, [ebx + 4]
+            stdcall GetYGLfloatCoord, eax
+            mov     [edi + 4], eax
+            add     edi, 8
+            ; circle coords
+            fldpi
+            fadd    st0, st0
+            fidiv   dword [degreeCount]
+            mov     ecx, [degreeCount]
+            inc     ecx
+            .drawDot:
+                fld     st0
+                fimul   [curentDeg]
+                fstp    [curentRudDeg]
+
+                ; x
+                fld     [curentRudDeg]
+                fcos
+                fimul   dword [ebx + 8]
+                fiadd   dword [ebx]
+                fstp    dword [edi]
+
+                ; y
+                fld     [curentRudDeg]
+                fsin
+                fimul   dword [ebx + 8]
+                fiadd   dword [ebx + 4]
+                fstp    dword [edi + 4]
+
+                ; convert
+                mov     eax, [edi]
+                stdcall GetXGLfloatCoordFromFloat, eax
+                mov     [edi], eax
+
+                mov     eax, [edi + 4]
+                stdcall GetYGLfloatCoordFromFloat, eax
+                mov     [edi + 4], eax
+
+                add     [curentDeg], 1
+                dec     ecx
+                add     edi, 8
+                cmp     ecx, 0
+                jne     .drawDot
+            mov     [curentDeg], 0
+            pop     ecx
+            add     ebx, 12
+            dec     ecx
+            jne     .convertCircle
+    .exit:
+        ret
+endp
+proc GetXGLfloatCoordFromFloat\ ; x/960.0-1.0
+     x
+    locals
+        res                 dd          0x00000000
+        floatNum            GLfloat     1.0
+        DeviceHalfWidth     GLfloat     960.0
+    endl
+        fld    dword [x]
+        fdiv    [DeviceHalfWidth]
+        fsub    [floatNum]
+        fstp    [res]
+        mov     eax, [res]
+    ret
+endp
+proc GetYGLfloatCoordFromFloat\ ; -(y/540.0-1.0)
+     y
+    locals
+        res                 dd          0x00000000
+        floatNum            GLfloat     1.0
+        DeviceHalfWidth     GLfloat     540.0
+        minus               dd          -1
+    endl
+        fld    dword [y]
+        fdiv    [DeviceHalfWidth]
+        fsub    [floatNum]
+        fimul   [minus]
+        fstp    [res]
+        mov     eax, [res]
     ret
 endp
 proc GetXGLfloatCoord\ ; x/960.0-1.0
@@ -83,22 +194,5 @@ proc GetYGLfloatCoord\ ; -(y/540.0-1.0)
         fimul   [minus]
         fstp    [res]
         mov     eax, [res]
-    ret
-endp
-
-proc Scripts.Getters.CovertIntToGLfloat\
-     value
-    ; x
-    mov     ebx, [value]
-    mov     eax, [ebx]
-    add     eax, 960
-    stdcall GetXGLfloatCoord, eax
-    mov     [ebx], eax
-    ; y
-    add     ebx, 4
-    mov     eax, [ebx]
-    add     eax, 540
-    stdcall GetYGLfloatCoord, eax
-    mov     [ebx], eax
     ret
 endp

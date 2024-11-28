4f74093ef55c1e370a68b7f59eef52ab6982d3fd
Kostya Betenya <betenya.kostya@mail.ru>
2024-11-28 19:29:53 +0300

    fix: change encodings fixes

--- builder/textParser.js
+++ builder/textParser.js
@@ -9,12 +9,22 @@ if (!inputString) {
 
 let encodedString = "";
 for (let i = 0; i < inputString.length; i++) {
-    if (inputString.charCodeAt(i) >= 'А'.charCodeAt(0) && inputString.charCodeAt(i) <= 'Я'.charCodeAt(0))
+    
+    
+    if (inputString.charCodeAt(i) >= 'А'.charCodeAt(0) && inputString.charCodeAt(i) <= 'Е'.charCodeAt(0))
         encodedString += (inputString.charCodeAt(i) - 'А'.charCodeAt(0) + 1) + ", ";
+    if (inputString.charCodeAt(i) >= 'а'.charCodeAt(0) && inputString.charCodeAt(i) <= 'е'.charCodeAt(0))
+        encodedString += (inputString.charCodeAt(i) - 'а'.charCodeAt(0) + 1) + ", ";
+    if (inputString.charCodeAt(i) == 'ё'.charCodeAt(0) || inputString.charCodeAt(i) == 'Ё'.charCodeAt(0))
+        encodedString += 7 + ", ";
+    if (inputString.charCodeAt(i) >= 'Ж'.charCodeAt(0) && inputString.charCodeAt(i) <= 'Я'.charCodeAt(0))
+        encodedString += (inputString.charCodeAt(i) - 'Ж'.charCodeAt(0) + 8) + ", ";
+    if (inputString.charCodeAt(i) >= 'ж'.charCodeAt(0) && inputString.charCodeAt(i) <= 'я'.charCodeAt(0))
+        encodedString += (inputString.charCodeAt(i) - 'ж'.charCodeAt(0) + 8) + ", ";
     if (inputString.charCodeAt(i) >= '0'.charCodeAt(0) && inputString.charCodeAt(i) <= '9'.charCodeAt(0))
         encodedString += (inputString.charCodeAt(i) - '0'.charCodeAt(0) + 34) + ", ";
     switch (inputString.charCodeAt(i)) {
-        case  ' '.charCodeAt(0): encodedString += 44 + ", "; break;
+        case  ' '.charCodeAt(0): encodedString += 44 + ",\\\n"; break;
         case  ','.charCodeAt(0): encodedString += 45 + ", "; break;
         case  '.'.charCodeAt(0): encodedString += 46 + ", "; break;
         case  '?'.charCodeAt(0): encodedString += 47 + ", "; break;
@@ -25,4 +35,4 @@ for (let i = 0; i < inputString.length; i++) {
 }
 encodedString += '0';
 
-console.log(`Закодированная строка: ${encodedString}`);
+console.log(`Закодированная строка: \n${encodedString}`);

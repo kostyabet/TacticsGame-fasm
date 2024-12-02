42d7a4e1455006c3b5f87cd443fc867d92693bbd
Kostya Betenya <betenya.kostya@mail.ru>
2024-12-02 22:04:12 +0300

    fix: exit from application

--- Application/_components/Exit.asm
+++ Application/_components/Exit.asm
@@ -3,9 +3,6 @@ proc Application.Exit
     stdcall File.TicksPosition.Write
     stdcall Application.FreeMamory
     invoke  HeapDestroy, [hHeap]
-    invoke  wglMakeCurrent,0,0
-    invoke  wglDeleteContext,[hrc]
-    invoke  ReleaseDC,[hwnd],[hdc]
-    invoke  PostQuitMessage,0
+    invoke  ExitProcess, 0
     ret
 endp
